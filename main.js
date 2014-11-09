fd4b359f06fd373fc1afd69d70b8747df03142fc
Greyhat Music <greyhatofficial@gmail.com>
2014-11-08 19:56:36 -0500

    styling

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-var movementStrength = 100;
-var  height = movementStrength / $(window).height();
-var width = movementStrength / $(window).width();
-
-$("html").mousemove(function(e){
-	
-	var pageX = e.pageX - ($(window).width() / 2);
-	var pageY = e.pageY - ($(window).height() / 2);
-	
-	var newvalueX = width * pageX * -1;
-	var newvalueY = height * pageY * -1;
-	
-	$('#centeredDecal img').css("left", newvalueX + "px");
-	$('#centeredDecal img').css("top", newvalueY + "px");
-	
-	$('#content').css("left", newvalueX * .2 + "px");
-	$('#content').css("top", newvalueY * .2 + "px");
-});
+$("#construction").fitText();
+$("#caption").fitText(2.0, { maxFontSize: '75px' });
+$("#small_caption").fitText(3.0, { maxFontSize: '75px' });
