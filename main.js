4ada25681f050b3cf60e81a92d79c272acd6ff9c
greyhatmusic <greyhatofficial@gmail.com>
2014-11-12 15:46:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 $("#construction").fitText();
 $("#caption").fitText(2.0, { maxFontSize: '75px' });
-$("#small_caption").fitText(3.0, { maxFontSize: '75px', minFontSize: '75px' });
+$("#small_caption").fitText(3.0, { maxFontSize: '75px', minFontSize: '20px' });
